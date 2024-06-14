35a97195c6aedfd91e7d305adf9eae82592d8d31
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-06-14 13:28:11 +0530

    dealer authentication implemented and some modification

--- src/main/java/com/dealerLoginServlet.java
+++ src/main/java/com/dealerLoginServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-public class adminLoginServlet extends HttpServlet {
+public class dealerLoginServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     private static final String JDBC_URL = "jdbc:mysql://localhost:3306/shandb";
@@ -20,28 +20,28 @@ public class adminLoginServlet extends HttpServlet {
     private static final String JDBC_PASSWORD = "Shan@1506";
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String email = request.getParameter("email");
+        String name = request.getParameter("name");
         String password = request.getParameter("password");
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection conn = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
 
-            String sql = "SELECT * FROM admin WHERE email = ? AND pwd = ?";
+            String sql = "SELECT * FROM dealer WHERE name = ? AND pwd = ?";
             PreparedStatement stmt = conn.prepareStatement(sql);
-            stmt.setString(1, email);
+            stmt.setString(1, name);
             stmt.setString(2, password);
 
             ResultSet rs = stmt.executeQuery();
 
             if (rs.next()) {
                 HttpSession session = request.getSession();
-                session.setAttribute("email", email);
-                response.sendRedirect("welcome-admin.jsp"); // Redirect to a welcome page after successful login
+                session.setAttribute("name", name);
+                response.sendRedirect("welcome-dealer.jsp"); // Redirect to a welcome page after successful login
             } else {
                 HttpSession session = request.getSession();
-                session.setAttribute("errorMessage", "Invalid email or password! Please try again.");
-                response.sendRedirect("admin-login.jsp");
+                session.setAttribute("errorMessage", "Invalid name or password! Please try again.");
+                response.sendRedirect("dealer-login.jsp");
             }
 
             conn.close();
