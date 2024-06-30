39e190ae7aa0cb88f06d290f28b73905e24f8a6d
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-06-30 10:53:24 +0530

    constraints upgraded

---
+++ SQL/uploaded_files.sql
@@ -0,0 +1,16 @@
+-- Create table
+
+CREATE TABLE uploaded_files (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    file_name VARCHAR(255) NOT NULL,
+    file_size INT NOT NULL,
+    file_type VARCHAR(100) NOT NULL,
+    stored_path VARCHAR(255) NOT NULL,
+    upload_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Describe table
+Desc uploaded_files;
+
+-- view all the records
+SELECT * FROM uploaded_files;
