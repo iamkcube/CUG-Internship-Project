3860024ff5cd44ece5fe755f5009704bf863adee
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-07-07 17:50:20 +0530

    Don't dare to change the sql scripts without my permission

--- SQL/users-table.sql
+++ SQL/users-table.sql
@@ -3,7 +3,7 @@
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(100) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
     contact VARCHAR(10) NOT NULL CHECK (LENGTH(contact) = 10 AND contact REGEXP '^[0-9]{10}$'),
     address VARCHAR(255) NOT NULL,
@@ -16,5 +16,12 @@ CREATE TABLE users (
 INSERT INTO users (username, email, password, contact, address, created_at, role) VALUES 
 ('admin1', 'admin1@gmail.com', SHA2('pass001', 256), '8260529733', 'Bhubaneswar', CURRENT_TIMESTAMP, 'admin'),
 ('admin2', 'admin2@gmail.com', SHA2('pass002', 256), '9437562924', 'Ghatika', CURRENT_TIMESTAMP, 'admin'),
+
 ('dealer1', 'dealer1@gmail.com', SHA2('pass01', 256), '6371929991', 'Kenjhor', CURRENT_TIMESTAMP, 'dealer'),
 ('dealer2', 'dealer2@gmail.com', SHA2('pass02', 256), '8895821654', 'Cuttack', CURRENT_TIMESTAMP, 'dealer');
+
+-- Describe table
+DESC users;
+
+-- View all the records of the table
+SELECT * FROM users;
