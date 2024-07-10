ba5a9a9c8c2f30c82790ebeb0e9f2379225bef4a
Shankar Singh Mahanty <shankarmahanty1412@gmail.com>
2024-07-11 01:21:32 +0530

    bill passing register

--- generate_pdf_bill.php
+++ generate_pdf_bill.php
@@ -200,8 +200,8 @@ $pdf->RenderTotals($totals);
 $pdf->RenderSummary($totals);
 $pdf->RenderSignature();
 
-// Output the PDF (download or display)
-$pdf->Output();
+// Output PDF to browser
+$pdf->Output('D', 'consolidated_cug_unit_bill.pdf');
 
 
 // Close database connection
